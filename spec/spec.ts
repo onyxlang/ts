46efcbe208c2923cd4a69ca8c69620b2367cd251
Vladislav Faust <hey@vladfaust.com>
2022-02-11 10:49:01 +0300

    feat: specify zig exe during spec

--- spec/spec.ts
+++ spec/spec.ts
@@ -1,14 +1,29 @@
 const compiler_exe = Deno.args[0];
 console.debug(`Running compiler specs for ${compiler_exe}`);
 
+const zig_exe = Deno.args[1];
+if (zig_exe) {
+  console.debug(`Using Zig at ${zig_exe}`);
+}
+
+function run(path: string) {
+  const cmd = [compiler_exe, "r", path];
+
+  if (zig_exe) {
+    cmd.push(...["--zig", zig_exe]);
+  }
+
+  return Deno.run({ cmd });
+}
+
 Deno.test("hello-world spec", async () => {
-  const p = Deno.run({ cmd: [compiler_exe, "r", "spec/hello-world.nx"] });
+  const p = run("spec/hello-world.nx");
   if (!(await p.status()).success) Deno.exit(1);
   p.close();
 });
 
 Deno.test("basic spec", async () => {
-  const p = Deno.run({ cmd: [compiler_exe, "r", "spec/basic.nx"] });
+  const p = run("spec/basic.nx");
   if (!(await p.status()).success) Deno.exit(1);
   p.close();
 });
